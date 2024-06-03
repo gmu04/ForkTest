c9919c6296f2ddb47d9c17dac478c52a79f647b1
Gokhan Mutlu <gokhanmutlu04@yahoo.com>
2024-06-03 14:29:25 +0300

    Initial Commit

---
+++ ForkTest/ForkTestApp.swift
@@ -0,0 +1,12 @@
+// ForkTestApp.swift by Gokhan Mutlu on 3.06.2024
+
+import SwiftUI
+
+@main
+struct ForkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
